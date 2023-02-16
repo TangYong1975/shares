ba9497c2f3972929c7e2972597e06224c3005db4
TangYong <11355664@qq.com>
2023-02-16 09:59:13 +0800

    1

---
+++ files/Add.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char** argv)
+{
+	if (argc == 4)
+	{
+		FILE* fp1 = fopen(argv[1], "rb");
+		FILE* fp2 = fopen(argv[2], "rb");
+		FILE* fp3 = fopen(argv[3], "wb");
+
+		long len1 = 0;
+		while (!feof(fp1))
+		{
+			fputc(fgetc(fp1), fp3);
+			len1++;
+		}
+		long len2 = 0;
+		while (!feof(fp2))
+		{
+			fputc((~fgetc(fp2)), fp3);
+			len2++;
+		}
+
+		fwrite(&len1, sizeof(long), 1, fp3);
+		fwrite(&len2, sizeof(long), 1, fp3);
+
+		fclose(fp3);
+		fclose(fp2);
+		fclose(fp1);
+	}
+}
---
+++ files/Sub.cpp
@@ -0,0 +1,28 @@
+﻿
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char** argv)
+{
+	if (argc == 3)
+	{
+		FILE* fp1 = fopen(argv[1], "rb");
+		FILE* fp2 = fopen(argv[2], "wb");
+
+		int len1 = 0;
+		int len2 = 0;
+
+		fseek(fp1, 0-sizeof(long), SEEK_END);
+		fread(&len2, sizeof(long), 1, fp1);
+
+		fseek(fp1, 0-2*sizeof(long), SEEK_END);
+		fread(&len1, sizeof(long), 1, fp1);
+
+		fseek(fp1, len1, SEEK_SET);
+		for(int i=0; i<len2; i++)
+			fputc((~fgetc(fp1)), fp2);
+
+		fclose(fp2);
+		fclose(fp1);
+	}
+}
